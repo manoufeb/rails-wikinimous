ab0d6372b6daab2a5495eef2b8df929142bc6c54
Manou <mfebvret@gmail.com>
2016-08-09 15:16:09 +0200

    articles are now creatable, editable, and deletable

--- Gemfile
+++ Gemfile
@@ -35,6 +35,7 @@ group :development, :test do
   gem 'byebug', platform: :mri
   gem "better_errors"
   gem "binding_of_caller"
+  gem "faker"
 end
 
 group :development do
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Article.create(name:Faker::Lorem.word, content:Faker::Lorem.paragraph)
+Article.create(name:Faker::Lorem.word, content:Faker::Lorem.paragraph)
+Article.create(name:Faker::Lorem.word, content:Faker::Lorem.paragraph)
+Article.create(name:Faker::Lorem.word, content:Faker::Lorem.paragraph)
+Article.create(name:Faker::Lorem.word, content:Faker::Lorem.paragraph)
+Article.create(name:Faker::Lorem.word, content:Faker::Lorem.paragraph)
+Article.create(name:Faker::Lorem.word, content:Faker::Lorem.paragraph)
+Article.create(name:Faker::Lorem.word, content:Faker::Lorem.paragraph)
+Article.create(name:Faker::Lorem.word, content:Faker::Lorem.paragraph)
+Article.create(name:Faker::Lorem.word, content:Faker::Lorem.paragraph)
